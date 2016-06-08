c3889a74c77d7f6ff4fc668a86e6444d0b1eb310
pascal <moraru.pasukaru@gmail.com>
2016-06-08 18:43:27 +0200

    first draft of dices screen

--- src/app/components/gamebook/dices/dices.controller.js
+++ src/app/components/gamebook/dices/dices.controller.js
@@ -1,6 +1,16 @@
+let ctrl;
 class DicesController {
     /*@ngInject*/
     constructor() {
+        ctrl = this;
+    }
+
+    roll1d6() {
+        alert('roll 1D6');
+    }
+
+    roll2d6() {
+        alert('roll 2D6');
     }
 }
 
