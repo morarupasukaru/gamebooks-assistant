e1649a48ff84c50f53ee86f3669dad2dd159675c
pascal <moraru.pasukaru@gmail.com>
2016-06-09 19:19:25 +0200

    first draft of items screen

--- src/app/components/gamebook/items/items.controller.js
+++ src/app/components/gamebook/items/items.controller.js
@@ -3,6 +3,12 @@ class ItemsController {
     /*@ngInject*/
     constructor() {
         ctrl = this;
+        ctrl.items = [
+            { quantity : 10, name : 'Meal', note: 'regain 4 stamina points'},
+            { quantity : 1, name : 'Sword'},
+            { quantity : 1, name : 'Shieldl'},
+            { quantity : 1, name : 'Leather armor'}
+        ];
     }
 
     addItem() {
