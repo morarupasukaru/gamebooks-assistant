95337f010cca5e19ca85c8c4c7bdc8ac9d0f3d40
pascal <moraru.pasukaru@gmail.com>
2016-06-14 17:07:28 +0200

    improve stats screen

--- src/app/components/gamebook/items/items.controller.js
+++ src/app/components/gamebook/items/items.controller.js
@@ -4,7 +4,7 @@ class ItemsController {
     constructor() {
         ctrl = this;
         ctrl.items = [
-            { quantity : 10, name : 'Meal', note: 'regain 4 stamina points'},
+            { quantity : 10, name : 'Meal (regain 4 stamina points)'},
             { quantity : 1, name : 'Sword'},
             { quantity : 1, name : 'Shield'},
             { quantity : 1, name : 'Leather armor'}
