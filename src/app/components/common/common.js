1f1be91bafe67182ffcb4a6b185d36acc5931164
pascal <moraru.pasukaru@gmail.com>
2016-06-22 18:23:05 +0200

    remove non-used code

--- src/app/components/common/common.js
+++ src/app/components/common/common.js
@@ -10,14 +10,11 @@
  */
 import angular from 'angular';
 import Home from './home/home';
-import About from './about/about';
 import Navbar from './navbar/navbar';
-import Posts from './posts/posts';
 import Messages from './messages/messages';
-import Theme from './theme/theme';
 
 let commonModule = angular.module('app.components.common', [
-    Home.name, About.name, Navbar.name, Posts.name, Messages.name, Theme.name
+    Home.name, Navbar.name, Messages.name
 ]);
 
 export default commonModule;
