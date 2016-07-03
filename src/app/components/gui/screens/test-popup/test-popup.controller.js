6fae0a5e0e85fc0cc24ffba14b04373f44f78df6
pascal <moraru.pasukaru@gmail.com>
2016-07-03 15:48:35 +0200

    test popup

--- src/app/components/gui/screens/test-popup/test-popup.controller.js
+++ src/app/components/gui/screens/test-popup/test-popup.controller.js
@@ -9,6 +9,15 @@ class TestPopupController {
         self.text = 'A question?'
         self.withCloseButton = false;
         self.choices = ['Yes', 'No'];
+        self.closeOnClickOutsideModal = false;
+
+        if (self.closeOnClickOutsideModal) {
+            window.onclick = function(event) {
+                if (!!event.target && !!event.target.id && event.target.id === self.modalElementId) {
+                    self.closePopup();
+                }
+            }
+        }
     }
 
     showPopup() {
@@ -16,7 +25,7 @@ class TestPopupController {
         modalElement.style.display = "block";
     }
 
-    closePopup(modalElementId) {
+    closePopup() {
         let modalElement = window.document.getElementById(self.modalElementId);
         modalElement.style.display = "none";
     }
