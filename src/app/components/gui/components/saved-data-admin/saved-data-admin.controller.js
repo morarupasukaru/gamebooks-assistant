49f9ef436f874c55f3452b9e54c1a639affe9fd1
pascal <moraru.pasukaru@gmail.com>
2016-07-04 14:00:23 +0200

    move export/import into configuration screen

--- src/app/components/gui/components/saved-data-admin/saved-data-admin.controller.js
+++ src/app/components/gui/components/saved-data-admin/saved-data-admin.controller.js
@@ -10,7 +10,7 @@ class SavedDataAdminController {
 
         self.popupConfirmEmptyLocalStorageConfig = {
             id : 'popupConfirmEmptyLocalStorage',
-            text : 'Are you sure to clear all of the content of the localStorage?',
+            text : 'Are you sure to clear the application data?',
             choices : [constants.choices.yes, constants.choices.no],
             withCloseButton : false,
             closeOnClickOutsideModal : false
@@ -19,14 +19,14 @@ class SavedDataAdminController {
 
     initData() {
         // TODO event on changes (take care of memory leak)
-        self.localStorageData = JSON.stringify(self.persistenceService.getAppDataFromLocalStorage());
+        self.applicationData = JSON.stringify(self.persistenceService.getAppDataFromLocalStorage());
     }
 
-    showPopupConfirmEmptyLocalStorage() {
-        self.popupService.show(self.popupConfirmEmptyLocalStorageConfig.id, self.callbackPopupConfirmEmptyLocalStorage);
+    showPopupConfirmDeleteApplicationData() {
+        self.popupService.show(self.popupConfirmEmptyLocalStorageConfig.id, self.callbackPopupConfirmDeleteApplicationData);
     }
 
-    callbackPopupConfirmEmptyLocalStorage(popupDomElementId, choice) {
+    callbackPopupConfirmDeleteApplicationData(popupDomElementId, choice) {
         if (choice === self.constants.choices.yes) {
             self.persistenceService.cleanAllData();
         }
--- src/app/components/gui/screens/screens.js
+++ src/app/components/gui/screens/screens.js
@@ -1,8 +1,6 @@
 import angular from 'angular';
 import Home from './home/home';
-import Export from './export/export';
 import Games from './games/games';
-import Import from './import/import';
 import Items from './items/items';
 import Notes from './notes/notes';
 import Paths from './paths/paths';
@@ -11,11 +9,10 @@ import Battle from './battle/battle';
 import Paragraph from './paragraph/paragraph';
 import Configuration from './configuration/configuration';
 import ChooseLanguage from './choose-language/choose-language';
-import TestPopup from './test-popup/test-popup';
 
 
 let screensModule = angular.module('app.components.gui.screens', [
-    Home.name, Export.name, Games.name, Import.name, Items.name, Notes.name, Paths.name, Stats.name, Battle.name, Paragraph.name, Configuration.name, ChooseLanguage.name, TestPopup.name
+    Home.name, Games.name, Items.name, Notes.name, Paths.name, Stats.name, Battle.name, Paragraph.name, Configuration.name, ChooseLanguage.name
 ]);
 
 export default screensModule;
