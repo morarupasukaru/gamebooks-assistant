ac30c66eb36fd4beed9f612aa21c717fb9099a6f
pascal <moraru.pasukaru@gmail.com>
2016-07-07 06:52:33 +0200

    add warlock book

--- src/app/components/services/books/warlock-of-firetop-mountain/warlock-of-firetop-mountain.service.js
+++ src/app/components/services/books/warlock-of-firetop-mountain/warlock-of-firetop-mountain.service.js
@@ -4,10 +4,83 @@ class WarlockOfFiretopMountainService {
     /*@ngInject*/
     constructor() {
         self = this;
+        self.book = {
+            name : 'The Warlock of Firetop Mountain',
+            authors : 'Steve Jackson & Ian Livingstone',
+            isbn :'0-7434-7511-9',
+            stats : [
+                {
+                    name : 'Skill',
+                    init: { sixDiceQuantity : 1, constant : 6}
+                },
+                {
+                    name : 'Stamina',
+                    init: { sixDiceQuantity : 2, constant : 12}
+                },
+                {
+                    name : 'Skill',
+                    init: { sixDiceQuantity : 1, constant : 6},
+                }
+            ],
+            items : [
+                {
+                    quantity : 1,
+                    name : 'sword'
+                },
+                {
+                    quantity : 1,
+                    name : 'shield'
+                },
+                {
+                    quantity : 1,
+                    name : 'leather armour'
+                },
+                {
+                    quantity : 1,
+                    name : 'backpack'
+                },
+                {
+                    quantity : 1,
+                    name : 'lantern'
+                },
+                {
+                    quantity : 10,
+                    name : 'meal (add 4 points to stamina)'
+                },
+                {
+                    quantity : 2,
+                    name : 'measure of potion of skill (restore skill points)'
+                },
+                {
+                    quantity : 2,
+                    name : 'measure of potion of strength (restore stamina points)'
+                },
+                {
+                    quantity : 2,
+                    name : 'measure of potion of luck (increase initial luck by 1 point and restore luck points)'
+                }
+            ],
+            paragraphs : [
+                {
+                    paragraphNr : null,
+                    description : 'Start',
+                    notes : [
+                        'Please choose either the potion of skill, strengh or luck (remove corresponding two unchoosen potions from items list.'
+                    ],
+                    choices : [
+                        {
+                            paragraphNr : 1,
+                            description : 'Go to Firetop Mountain'
+                        }
+                    ]
+                }
+            ],
+            notes : []
+        };
     }
 
     getBook() {
-        return {};
+        return self.book;
     }
 }
 
