76294709c5c70e4058a7022eb7a27122b44aa75b
pascal <moraru.pasukaru@gmail.com>
2016-07-11 15:24:16 +0200

    convert screen stats as component

--- src/app/components/gui/screens/in-game/in-game.controller.js
+++ src/app/components/gui/screens/in-game/in-game.controller.js
@@ -13,6 +13,36 @@ class InGameController {
             ]
         };
         this.notes = [ {note:'note 1'}, {note:'note 2', playerName : 'Pascal'}, {note:'note 3', playerName: 'François' } ];
+        this.items = [
+            {
+                quantity : 1,
+                description : 'sword'
+            },
+            {
+                quantity : 1,
+                description : 'shield'
+            },
+            {
+                quantity : 1,
+                description : 'leather armour'
+            },
+            {
+                quantity : 1,
+                description : 'backpack'
+            },
+            {
+                quantity : 1,
+                description : 'lantern'
+            },
+            {
+                quantity : 10,
+                description : 'meal (add 4 points to stamina)'
+            },
+            {
+                quantity : 2,
+                description : 'measure of potion of skill (restore skill points)'
+            }
+        ];
     }
 
     addRow() {
