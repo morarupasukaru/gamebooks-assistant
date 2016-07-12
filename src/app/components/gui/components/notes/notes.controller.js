22f3bdb778933ea13a08f009fc18f23833f831eb
pascal <moraru.pasukaru@gmail.com>
2016-07-12 17:16:50 +0200

    add popup for delete action

--- src/app/components/gui/components/notes/notes.controller.js
+++ src/app/components/gui/components/notes/notes.controller.js
@@ -1,14 +1,38 @@
 let self;
 class NotesController {
     /*@ngInject*/
-    constructor(preScreenLoadingInterceptorsCallerService) {
+    constructor(preScreenLoadingInterceptorsCallerService, popupService, constants) {
         self = this;
         preScreenLoadingInterceptorsCallerService.intercept();
+
+        self.popupService = popupService;
+        self.constants = constants;
+
+        self.popupDeleteNoteConfig = {
+            id : 'popupDeleteNoteConfig',
+            text : 'Are you sure to remove the note?',
+            choices : [constants.choices.yes, constants.choices.no],
+            withCloseButton : false,
+            closeOnClickOutsideModal : false
+        };
+
         this.rows = this.notes;
     }
 
     addRow(noteValue) {
         self.rows.push({ note : noteValue, paragraphNumber : Number(self.paragraphNumber), playerName : self.playerName });
+    }
+
+    displayRemovePopup(removedRow) {
+        self.rowToBeRemoved = removedRow;
+        self.popupService.show(self.popupDeleteNoteConfig.id, self.callbackRemovePopup);
+    }
+
+    callbackRemovePopup(popupDomElementId, choice) {
+        if (choice === self.constants.choices.yes) {
+            self.removeRow(self.rowToBeRemoved);
+        }
+        self.rowToBeRemoved = null;
     }
 
     removeRow(removedRow) {
