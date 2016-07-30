f7fecef0927e0adecfcd358ab1672ad6fd95a216
pascal <moraru.pasukaru@gmail.com>
2016-07-30 14:15:38 +0200

    use gameService in "in-game" screen

--- src/app/components/gui/screens/in-game/in-game.controller.js
+++ src/app/components/gui/screens/in-game/in-game.controller.js
@@ -1,12 +1,29 @@
 let self;
 class InGameController {
     /*@ngInject*/
-    constructor(preScreenLoadingInterceptorsCallerService, $location, constants, endGamePopupService) {
+    constructor(preScreenLoadingInterceptorsCallerService, $location, constants, endGamePopupService, gamesService, $stateParams) {
         self = this;
         preScreenLoadingInterceptorsCallerService.intercept();
         self.$location = $location;
         self.constants = constants;
         self.endGamePopupService = endGamePopupService;
+        self.gamesService = gamesService;
+        self.$stateParams = $stateParams;
+        if (!!$stateParams.game) {
+            this.buildGame($stateParams.game);
+            self.game = self.gamesService.getGame(decodeURIComponent($stateParams.game));
+        }
+        this.notes = [];
+        if (!!self.game) {
+            this.playerName = self.game.playerName;
+            if (!!self.game.notes) {
+                this.notes = this.notes.concat(self.game.notes);
+            }
+            this.items = [];
+            if (!!self.game.items) {
+                this.items = this.items.concat(self.game.items);
+            }
+        }
         this.paragraph = {
             paragraphNumber : 1,
             description : 'Start of the game\nStart of the game\nStart of the game\nStart of the game\nStart of the game\nStart of the game\n',
@@ -17,39 +34,48 @@ class InGameController {
         };
 
         self.popupAbandonGameConfig = { id : 'popupAbandonGame' };
+    }
 
-        this.notes = [ {note:'note 1'}, {note:'note 2', playerName : 'Pascal'}, {note:'note 3', playerName: 'François', paragraphNumber : 123 } ];
-        this.items = [
-            {
-                quantity : 1,
-                description : 'sword'
-            },
-            {
-                quantity : 1,
-                description : 'shield'
-            },
-            {
-                quantity : 1,
-                description : 'leather armour'
-            },
-            {
-                quantity : 1,
-                description : 'backpack'
-            },
-            {
-                quantity : 1,
-                description : 'lantern'
-            },
-            {
-                quantity : 10,
-                description : 'meal (add 4 points to stamina)'
-            },
-            {
-                quantity : 2,
-                description : 'measure of potion of skill (restore skill points)'
-            }
-        ];
-        this.playerName = 'Donald';
+    buildGame(gameId) {
+        // TODO remove
+        let game = {
+            id : gameId,
+            playerName : 'Pascal',
+            bookUrlName : 'warlock-firetop-mountain',
+            currentParagraphNr : '0',
+            notes : [ {note:'note 1'}, {note:'note 2', playerName : 'Pascal'}, {note:'note 3', playerName: 'François', paragraphNumber : 123 } ],
+            items : [
+                        {
+                            quantity : 1,
+                            description : 'sword'
+                        },
+                        {
+                            quantity : 1,
+                            description : 'shield'
+                        },
+                        {
+                            quantity : 1,
+                            description : 'leather armour'
+                        },
+                        {
+                            quantity : 1,
+                            description : 'backpack'
+                        },
+                        {
+                            quantity : 1,
+                            description : 'lantern'
+                        },
+                        {
+                            quantity : 10,
+                            description : 'meal (add 4 points to stamina)'
+                        },
+                        {
+                            quantity : 2,
+                            description : 'measure of potion of skill (restore skill points)'
+                        }
+                    ]
+        };
+        self.gamesService.addGame(game);
     }
 
     startBattle() {
