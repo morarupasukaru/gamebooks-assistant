6481ca98104fae3f2f6d5e64e2c6a52de666c360
pascal <moraru.pasukaru@gmail.com>
2016-08-08 16:16:27 +0200

    upload constants

--- src/app/components/constants/constants.js
+++ src/app/components/constants/constants.js
@@ -3,7 +3,7 @@ import angular from 'angular';
 /*@ngInject*/
 let constantsModule = angular.module('app.components.constants', [])
     .constant('constants', {
-        version: '20160807',
+        version: '20160808',
         supportedLanguages: ['en', 'fr'],
         data: {
             selectedLanguage: 'selectedLanguage',
