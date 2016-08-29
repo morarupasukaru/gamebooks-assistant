e3c7dc03eadc0bc54caa653e8cd1fd2c34b0213a
pascal <moraru.pasukaru@gmail.com>
2016-08-29 17:15:47 +0200

    update version

--- src/app/components/constants/constants.js
+++ src/app/components/constants/constants.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 
 let constantsModule = angular.module('app.components.constants', [])
     .constant('constants', {
-        version: '20160822',
+        version: '20160829',
         supportedLanguages: ['en', 'fr'],
         data: {
             selectedLanguage: 'selectedLanguage',
