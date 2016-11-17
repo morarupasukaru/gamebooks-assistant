bf81a830ff60348302b287323e83835362fb5fe9
pascal <moraru.pasukaru@gmail.com>
2016-11-17 06:51:40 +0100

    map (work in progress)

--- src/app/components/gui/components/nodes/node/node.controller.js
+++ src/app/components/gui/components/nodes/node/node.controller.js
@@ -2,6 +2,16 @@ class NodeController {
     /*@ngInject*/
     constructor() {
     }
+
+    toggleCollapse(node) {
+        if (!!node.children) {
+            if (!node.collapsed) {
+                node.collapsed = true;
+            } else {
+                node.collapsed = !node.collapsed;
+            }
+        }
+    }
 }
 
 export default NodeController;
--- src/app/components/gui/components/nodes/nodes.controller.js
+++ src/app/components/gui/components/nodes/nodes.controller.js
@@ -8,11 +8,35 @@ class NodesController {
 
     initData() {
         this.rootNode = this.mapPersistenceService.getMap(this.adventureId, this.rootParagraphNr);
+        this.collapseAll(this.rootNode)
+        this.expand(this.rootNode, 2);
+    }
+
+    collapseAll(node) {
+        this.collapse(node, -1);
+    }
+
+    collapse(node, level) {
+        this.setCollapse(node, level, true);
+    }
+
+    expand(node, level) {
+        this.setCollapse(node, level, false);
+    }
+
+    setCollapse(node, level, collapse) {
+        if (!!node.children) {
+            node.collapsed = collapse;
+            if (level !== 0) {
+                for (let i = 0; i < node.children.length; i++) {
+                    this.setCollapse(node.children[i], level-1, collapse);
+                }
+            }
+        }
     }
 
     /*
                 checked: false,
-                collapsed: false,
                 selected: false
 */
 }
