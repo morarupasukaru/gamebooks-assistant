a0034c40d2ccdc2a6773839f69d58c6e3c80c4fc
pascal <moraru.pasukaru@gmail.com>
2016-12-22 17:56:10 +0100

    merge player stats & characters

--- src/app/components/gui/components/characters/characters.controller.js
+++ src/app/components/gui/components/characters/characters.controller.js
@@ -9,49 +9,21 @@ class CharactersController {
     initData() {
         this.game = this.gamePersistenceService.getGame(decodeURIComponent(this.gameId));
         this.innerEntries = [];
-        this.innerEntries.push({ value: this.displayValueForPlayer(this.game), deletable : false });
+        this.innerEntries.push({
+            value: this.game.playerName,
+            stats: this.game.stats,
+            deletable : false
+        });
         if (this.game.characters) {
             for (let i = 0; i < this.game.characters.length; i++) {
-                this.innerEntries.push({ value: this.displayValueForCharacter(this.game.characters[i]), deletable : true });
+                this.innerEntries.push({
+                    value: this.game.characters[i].name,
+                    stats: this.game.characters[i],
+                    deletable : true
+                });
             }
         }
     }
-
-    displayValueForPlayer(game) {
-        let value = game.playerName;
-        if (!!game.stats && game.stats.length > 0) {
-            value = value + ' - ';
-            for (let i = 0; i < game.stats.length; i++) {
-                let stats = game.stats[i];
-                let statsValue = stats.name + ' ['+ stats.current + '/' + stats.initial + ']';
-                value = value + statsValue;
-                if (i < game.stats.length - 1) {
-                    value = value + ', ';
-                }
-            }
-        }
-        return value;
-    }
-
-    displayValueForCharacter(character) {
-        let value = character.name;
-        let keys  = Object.keys(character);
-        keys.splice(keys.indexOf('name'), 1);
-        keys.splice(keys.indexOf('$$hashKey$$'), 1);
-        if (keys .length > 0) {
-            value = value + ' - ';
-            for (let i = 0; i < keys.length; i++) {
-                let key = keys[i];
-                let statsValue = key + ' ['+ character[key] + ']';
-                value = value + statsValue;
-                if (i < keys.length - 1) {
-                    value = value + ', ';
-                }
-            }
-        }
-        return value;
-    }
-
 
     addEntry() {
         this.innerEntries.push({ value: this.newEntry });
