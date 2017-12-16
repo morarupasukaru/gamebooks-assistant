b9be36aee7f596b0870df4bc2ca1c4c18b887081
morarupasukaru <0nlyf0rfun>
2017-12-16 13:06:13 +0100

    integrate javascript into build process

--- src/mobile-version/Gruntfile.js
+++ src/mobile-version/Gruntfile.js
@@ -1,9 +1,11 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        target: 'target',
+
         // Clean temporary folders
         clean: {
-            before: ['target'],
-            after: ['target/html']
+            before: ['<%= target %>'],
+            after: ['<%= target %>/html']
         },
 
         // Concat css files into a single minified css file
@@ -51,7 +53,7 @@ module.exports = function(grunt) {
                         'css/i18n.css',
                         'html/**/*.css'
                     ],
-                    dest: 'target/assets/styles.css'
+                    dest: '<%= target %>/assets/styles.css'
                 }]
             }
         },
@@ -71,7 +73,7 @@ module.exports = function(grunt) {
                 'assets/skeleton/**/*.css',
                 'css/**/*.css',
                 'html/**/*.css',
-                'target/**/*.css'
+                '<%= target %>/**/*.css'
             ]
           },
           lax: {
@@ -85,17 +87,34 @@ module.exports = function(grunt) {
                 'assets/skeleton/**/*.css',
                 'css/**/*.css',
                 'html/**/*.css',
-                'target/**/*.css'
+                '<%= target %>/**/*.css'
             ]
           }
         },
+
+        jshint: {
+            files: ['js/**/*.js'],
+            options: {
+                jshintrc: true
+            }
+        },
+
+        uglify: {
+            options: {},
+            my_target: {
+              files: {
+                '<%= target %>/assets/app.js': ['js/**/*.js']
+              }
+            }
+        },
+
         copy: {
           main: {
             files: [
               // includes files within path
-              { expand: true, flatten: true, src: ['assets/icons/icomoon/fonts/*'], dest: 'target/assets/fonts', filter: 'isFile'},
+              { expand: true, flatten: true, src: ['assets/icons/icomoon/fonts/*'], dest: '<%= target %>/assets/fonts', filter: 'isFile'},
 
-              { expand: true, flatten: true, src: 'assets/favicon/favicon.ico', dest: 'target/'}
+              { expand: true, flatten: true, src: 'assets/favicon/favicon.ico', dest: '<%= target %>/'}
             ],
           },
         },
@@ -106,7 +125,7 @@ module.exports = function(grunt) {
                   expand: true,     // Enable dynamic expansion.
                   cwd: 'html/',      // Src matches are relative to this path.
                   src: ['**/*.html'], // Actual pattern(s) to match.
-                  dest: 'target/',   // Destination path prefix.
+                  dest: '<%= target %>/',   // Destination path prefix.
                 }
               ]
             }
@@ -119,7 +138,7 @@ module.exports = function(grunt) {
                 // connect to a validator instance running in server mode on localhost:8888
                 server: {}
               },
-              src: ['html/**/*.html', 'target/**/*.html']
+              src: ['html/**/*.html', '<%= target %>/**/*.html']
             }
         },
         htmlmin: {
@@ -130,9 +149,9 @@ module.exports = function(grunt) {
                 },
                 files: [{
                   expand: true,
-                  cwd: 'target',
+                  cwd: '<%= target %>',
                   src: ['**/*.html'],
-                  dest: 'target/'
+                  dest: '<%= target %>/'
               }]
             }
         },
@@ -151,11 +170,16 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-processhtml');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-html');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['vnuserver', 'buildPipeline', 'watch']);
-    grunt.registerTask('buildPipeline', ['clean:before', 'cssmin', 'csslint:strict', 'copy', 'processhtml', 'htmlmin', 'htmllint', 'clean:after']);
+    grunt.registerTask('buildPipeline', ['clean:before', 'cssmin', 'csslint:strict', 'jshint', 'uglify', 'copy', 'processhtml', 'htmlmin', 'htmllint', 'clean:after']);
 };
+
+// copy to distrib
+// rm -Rf ../../../gamebooks-assistant-dist/assets && rm -Rf ../../../gamebooks-assistant-dist/confirmation && rm -Rf ../../../gamebooks-assistant-dist/gamebook && rm -Rf ../../../gamebooks-assistant-dist/gamebooks && rm -Rf ../../../gamebooks-assistant-dist/*.* && cp -R target/*.* ../../../gamebooks-assistant-dist && cp -R target/assets ../../../gamebooks-assistant-dist/assets && cp -R target/confirmation ../../../gamebooks-assistant-dist/confirmation && cp -R target/gamebook ../../../gamebooks-assistant-dist/gamebook && cp -R target/gamebooks ../../../gamebooks-assistant-dist/gamebooks
--- src/mobile-version/js/i18n.js
+++ src/mobile-version/js/i18n.js
@@ -1,7 +1,13 @@
+/**
+ * Application initialisation
+ */
 $(function() {
   initLanguage();
 });
 
+/**
+ * Initialise the language of the application based on the language configuration of the browser
+ */
 function initLanguage() {
     var language = navigator.languages && navigator.languages[0] || navigator.language || navigator.userLanguage;
     if (language === 'fr' || language.startsWith('fr-')) {
@@ -11,14 +17,22 @@ function initLanguage() {
     }
 }
 
-function setLanguage(language) {
+/**
+ * Change the language of the application
+ */
+function setLanguage(newLanguage) {
     var currentLanguage = $("html").attr("lang");
     if (!currentLanguage) {
         currentLanguage = "en";
     }
-    if (currentLanguage !== language) {
-        $("html").attr("lang", language);
+    if (currentLanguage !== newLanguage) {
+        $("html").attr("lang", newLanguage);
         $("#link_" + currentLanguage).toggleClass("hidden");
-        $("#link_" + language).toggleClass("hidden");
+        $("#link_" + newLanguage).toggleClass("hidden");
+        reloadCSS();
     }
+}
+
+function reloadCSS() {
+  $('#i18n-css').replaceWith('<link id="i18n-css" rel="stylesheet" href="assets/i18n.css?t=' + Date.now() + '">');
 }
