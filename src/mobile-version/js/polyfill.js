0709993dc60cce10e33db9bc36824d940e400622
morarupasukaru <0nlyf0rfun>
2018-02-12 08:19:57 +0100

    encapsulate polyfill.js content into a global function (similar to other files)

--- src/mobile-version/js/polyfill.js
+++ src/mobile-version/js/polyfill.js
@@ -3,11 +3,13 @@
  * see https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/String/startsWith
  */
 // jshint freeze:false
-if (!String.prototype.startsWith) {
-    String.prototype.startsWith = function (searchString, position) {
-        "use strict";
-        position = position || 0;
-        return this.substr(position, searchString.length) === searchString;
-    };
-}
+(function(){
+    "use strict";
+    if (!String.prototype.startsWith) {
+        String.prototype.startsWith = function (searchString, position) {
+            position = position || 0;
+            return this.substr(position, searchString.length) === searchString;
+        };
+    }
+} (this));
 // jshint freeze:true
