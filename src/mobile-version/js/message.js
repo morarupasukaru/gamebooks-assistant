8310b403914ae035af098623db3b93933d41fff9
morarupasukaru <0nlyf0rfun>
2018-02-12 11:31:03 +0100

    dispay non translated text

--- src/mobile-version/js/message.js
+++ src/mobile-version/js/message.js
@@ -23,13 +23,16 @@
 
     api._displayMessage = function(severity, message) {
         var language = _.i18n.currentLanguage;
-        var i18nMessage = _.config.translatedTexts[language][message];
+        var displayedMessage = _.config.translatedTexts[language][message];
+        if (!displayedMessage) {
+            displayedMessage = message;
+        }
         var i18nTitle = _.config.translatedTexts[language][_.config.texts.modalTitlePrefix + severity];
 
         var modalTitle = window.document.getElementById(this.ids.title);
         modalTitle.innerText = i18nTitle;
         var modalText = window.document.getElementById(this.ids.text);
-        modalText.innerText = i18nMessage;
+        modalText.innerText = displayedMessage;
 
         var modalElement = window.document.getElementById(this.ids.modal);
         modalElement.style.display = "block";
@@ -40,6 +43,9 @@
         modalElement.style.display = "none";
     };
 
+    /**
+     * Exception handling: uncatched exception are displayed in a modal view
+     */
     if (!!globals.addEventListener) {
         globals.addEventListener('error', function (e) {
             _.msg.error(e.error.toString());
