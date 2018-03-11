13f798e5b2bd1b065f5b47c824e6c61a7ecdd88e
pasukaru <moraru.pasukaru@gmail.com>
2018-03-11 09:42:26 +0100

    improve tests

--- src/cypress/integration/gamebooks_screen_spec.js
+++ src/cypress/integration/gamebooks_screen_spec.js
@@ -93,6 +93,60 @@ describe('Gamebook Selection screen', function () {
 			cy.get('#screen-gamebooks-adminBtn').click();
 			cy.modalFeatureNotImplemented();
 		})
+	}),
+
+	context('Tests with data loading', function () {
+		it('No popup message with offline & no data in localStorage & if not specified remote url for data', function() {
+			// TODO
+		}),
+		
+		it('No popup message with online & no data in localStorage & if not specified remote url for data', function() {
+			// TODO
+		}),
+		
+		it('Popup message with offline & no data in localStorage & specified remote url for data', function() {
+			// TODO
+		}),
+		
+		it('Display localStorage data if offline', function() {
+			// TODO
+		}),
+		
+		it('localStorage data = loaded data', function() {
+			// TODO
+		}),
+		
+		it('unmodified localStorage gamebooks deleted if not anymore in loaded data', function() {
+			// TODO
+		}),
+		
+		it('new gamebooks created by the application is never deleted if not found in loaded data', function() {
+			// TODO
+		}),
+		
+		it('popup to ask to delete modified localStorage gamebook deleted if not anymore in loaded data -> no choice', function() {
+			// TODO with several deletion
+		}),
+		
+		it('popup to ask to delete modified localStorage gamebook deleted if not anymore in loaded data -> yes choice', function() {
+			// TODO
+		}),
+		
+		it('unmodified localStorage gamebooks replaced by new version from loaded data', function() {
+			// TODO
+		}),
+		
+		it('popup to ask to replace the modified localStorage gamebook by the new version in loaded data -> no choice', function() {
+			// TODO with several deletion
+		}),
+		
+		it('popup to ask to replace the modified localStorage gamebook by the new version in loaded data -> yes choice', function() {
+			// TODO
+		}),
+		
+		it('add new gamebook from loaded data in localStorage', function() {
+			// TODO
+		})
 	})
 	
 });
